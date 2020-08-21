3e3bb55a33cca3b40ef460c5aa36329861ae8f04
thisisjofrank <jofranchetti@gmail.com>
2020-08-21 15:56:06 +0100

    remove everything

--- api/init/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = async function (context, req) {
-    context.res = {
-        headers: { "content-type": "application/json" },
-        body: { status: "ok" }
-    };
-};
--- app/js/components/Loader.js
+++
@@ -1,8 +0,0 @@
-export const Loader = {
-    template: `
-    <div class="loader">
-    <h2 class="section-heading">Creating a game</h2>
-     <img src="../../assets/loading.gif" alt="loading" class="loader-gif" />
-    </div>
-`
-};
